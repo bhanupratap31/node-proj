bdec5de2a0ba401af2c554668fcbe1a1c04ec32a
Bhanu Pratap Shukla <bhanu@Bhanus-MacBook-Pro.local>
2024-05-20 22:51:18 +0530

    middlewares

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const http = require('http'); 
-
 const express = require('express');
 
 const app = express(); 
 
-app.use((req, res, next)=>{
-    console.log('In the middleware!');
-    next(); 
-});
-app.use((req, res, next)=>{
-    console.log('In the middleware!');
+app.use('/add-product',(req, res, next)=>{
+    console.log('In the first middleware!');
+    res.send('<h1>On the add-product page</h1>');
 });
 
-const server = http.createServer(app);
+app.use('/',(req, res, next)=>{
+    console.log('In the next middleware!');
+    res.send('<h1>Hello from express</h1>');
+});
 
-
-server.listen(3000);
+app.listen(3000);
