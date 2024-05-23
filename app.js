38e258a2a2a83f64b1a70c9f1c405edb5f001d4f
Bhanu Pratap Shukla <bhanu@Bhanus-MacBook-Pro.local>
2024-05-23 22:06:08 +0530

    Adding routes and page not found

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+const bodyParser = require('body-parser');
 
 const app = express(); 
 
-app.use('/add-product',(req, res, next)=>{
-    console.log('In the first middleware!');
-    res.send('<h1>On the add-product page</h1>');
-});
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req, res, next)=>{
-    console.log('In the next middleware!');
-    res.send('<h1>Hello from express</h1>');
+app.use((req, res, next) =>{
+    res.status(404).send('<h1>Page not found</h1>')
 });
 
 app.listen(3000);
