213bc692766f72fc8e0f995aa312dfd3c431119c
Bhanu Pratap Shukla <bhanu@Bhanus-MacBook-Pro.local>
2024-05-29 22:46:30 +0530

    Pug and dynamic template engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,15 @@
 const path = require('path');
-const express = require('express'); 
-const rootDir = require('../util/path');
-const router = express.Router(); 
 
-router.get('/',(req, res, next)=>{
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {prods: products, docTitle: 'Shop'});
 });
 
-module.exports = router; 
+module.exports = router;
